5b4bfe6400c9b12bd18cd0344c1f8b321857fc41
JOEzeros <135394583+JOEzeros@users.noreply.github.com>
2023-09-25 09:22:21 -0700

    Create llll.lua

---
+++ llll.lua
@@ -0,0 +1,6 @@
+return {
+  ["FREE"] = true,
+  ["PAID"] = true,
+  ["FREE ERROR"] = "Free Is Disabled by the Owner. Please Contact the Owner for more Informations",
+  ["PAID ERROR"] = "Paid Is Disabled by the Owner. Please Contact the Owner for more Informations",
+}
